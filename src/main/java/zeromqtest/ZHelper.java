07859619a123e1f6d9676d768c6db33f33597076
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2016-02-02 16:18:58 +0100

    missing class

---
+++ src/main/java/zeromqtest/ZHelper.java
@@ -0,0 +1,62 @@
+package zeromqtest;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import org.zeromq.ZMQ;
+import org.zeromq.ZMQ.Context;
+import org.zeromq.ZMQ.Socket;
+
+public class ZHelper
+{
+    private static Random rand = new Random(System.currentTimeMillis ());
+
+    /**
+     * Receives all message parts from socket, prints neatly
+     */
+    public static void dump (final Socket sock)
+    {
+        System.out.println("----------------------------------------");
+        while(true) {
+            final byte [] msg = sock.recv (0);
+            boolean isText = true;
+            String data = "";
+            for (int i = 0; i< msg.length; i++) {
+                if (msg[i] < 32 || msg[i] > 127)
+                    isText = false;
+                data += String.format ("%02X", msg[i]);
+            }
+            if (isText)
+                data = new String (msg);
+
+            System.out.println (String.format ("[%03d] %s", msg.length, data));
+            if (!sock.hasReceiveMore ())
+                break;
+        }
+    }
+
+    public static void setId (final Socket sock)
+    {
+        final String identity = String.format ("%04X-%04X", rand.nextInt (), rand.nextInt ());
+
+        sock.setIdentity (identity.getBytes ());
+    }
+
+    public static List<Socket> buildZPipe(final Context ctx) {
+        final Socket socket1 = ctx.socket(ZMQ.PAIR);
+        socket1.setLinger(0);
+        socket1.setHWM(1);
+
+        final Socket socket2 = ctx.socket(ZMQ.PAIR);
+        socket2.setLinger(0);
+        socket2.setHWM(1);
+
+        final String iface = "inproc://" + new BigInteger(130, rand).toString(32);
+        socket1.bind(iface);
+        socket2.connect(iface);
+
+        return Arrays.asList(socket1, socket2);
+    }
+}
